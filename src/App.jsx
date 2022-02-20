62d50e9d2f353873407d2dbe801117f20ea2ae48
Fr33yr <75025925+Fr33yr@users.noreply.github.com>
2022-02-19 22:25:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Fragment, useState, useRef, useEffect } from "react";
+//import { uuid } from 'uuidv4';
+
+import './App.css'
+
+export function App() {
+
+
+    return (
+        <Fragment>
+            <div className="slots-container">
+                <input type="checkbox" />
+                <input type="checkbox" />
+                <input type="checkbox" />
+                <input type="checkbox" />
+                <p>1ST</p>
+            </div>
+        </Fragment>
+    )
+}
